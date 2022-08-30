fa43b3c2b1d7d00343a976e61847bf063e0e55fa
jiwon <sjgirls88@naver.com>
2022-08-30 13:45:39 +0900

    수정3

--- myapp3/src/main/java/com/kh/myapp3/ProductController.java
+++ myapp3/src/main/java/com/kh/myapp3/ProductController.java
@@ -4,6 +4,7 @@ package com.kh.myapp3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,7 +25,31 @@ public class ProductController {
   @PostMapping()
   public String saver(){
 
-    return "product/all";   //전체목록 view
+    return "redirect:/product/1";   //상품상세 view
 
   }
+
+  //상품개별조회
+  @GetMapping("/{pid}")
+  public String findByProductId(@PathVariable("pid") String pid){
+    //db에서 상품조회
+
+    return "product/detailForm";    //상품 상제 view
+  }
+
+  //수정양식
+  @GetMapping("/{pid}/edit")      //위에랑 식별이 안돼서 edit 붙여줌
+  public String updateForm(){
+
+    return "product/updateForm";      //상품 수정 view
+  }
+
+
+  //수정처리.
+  @PostMapping("/{pid}/edit")
+  public String update(){
+
+    return "redirect:/product/1";     //상품 상세 view
+  }
+
 }
